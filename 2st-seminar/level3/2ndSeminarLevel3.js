3040b11e31d194dd96eed7cd744897259ab903dd
Jihyun247 <wlguslove12345@gmail.com>
2020-12-23 23:06:22 +0900

    [ADD] 2st seminar level3 fs 모듈 이용해서 비동기적으로 비번 암호화

--- 2st-seminar/level3/2ndSeminarLevel3.js
+++ 2st-seminar/level3/2ndSeminarLevel3.js
@@ -6,7 +6,7 @@ const fileName = 'useFSpwd';
 
 crypto.randomBytes(64, (err, buf) => {
     // buf는 버퍼형식이기 때문에 base64 salt로 변경
-    crypto.pbkdf2('비밀번호', buf.toString('base64'), 100000, 64, 'sha512', (err, key) => {
+    crypto.pbkdf2(pwd, buf.toString('base64'), 100000, 64, 'sha512', (err, key) => {
         fs.writeFile(`${fileName}.txt`, key.toString('base64'), () => {
             console.log(`file[${fileName}] write complete`);
         });
